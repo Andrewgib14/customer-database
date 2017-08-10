8e640cb7b82e741d2450f2d760a543b051c580b0
Andrew Gibson <andrewgib14@gmail.com>
2017-08-10 14:33:52 -0500

    added loop to populate html

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-for (i = 0; i < 3; i++) {
-    for (x = 0; x < 4; x++) {
-    }
-    for (y = 0; x < 4; x++) {
-
-    }
-}
-
-for (i = 0; i < customers.length;)
+let container = document.querySelector(".container");
+for (let i = 0; i < customers.results.length; i++) {
+    let content =
+        `<div class='content'>
+        <img class="img" src="${customers.results[i].picture.large}" alt="">
+        <h2>${customers.results[i].name.first} ${customers.results[i].name.last}</h2>
+        <hr>
+        <p>${customers.results[i].email}</p>
+        <br>
+        <p>${customers.results[i].location.street}</p>
+        <p>${customers.results[i].location.city},${customers.results[i].location.state} ${customers.results[i].location.zip}</p>
+        <p>${customers.results[i].phone}</p>
+        <p class= "ss">${customers.results[i].value}</p>
+    </div>`;
+    container.innerHTML += content;
+};
